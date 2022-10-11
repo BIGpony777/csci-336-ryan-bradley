199aaa1cafafc3f735c8c30f1fe97b769e17eea8
Ryan Bradley <BIGpony777@gmail.com>
2022-10-11 16:59:40 -0400

    switched hide to remove

--- assignments/jquery/movies/javascript.js
+++ assignments/jquery/movies/javascript.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  
+
   $("#movie-list").on("click",".remove-movie-btn", function() {  
-    $(this).closest("div").hide();
+    $(this).closest("div").remove();
   });
 
   $("#clear-movies-btn").on("click", function() {
-    $(".movie-name").closest("div").hide();
+    $(".movie-name").closest("div").remove();
   });
 
   $("#add-movie-btn").on("click", function() {   
